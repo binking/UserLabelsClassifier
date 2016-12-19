760c8945c82332a876ae6e06a02888398b608cab
binking <jiangzhibin2014.xujie@gmail.com>
2016-12-19 23:57:12 +0800

    m nb

--- compare_multiple_models.py
+++ compare_multiple_models.py
@@ -140,7 +140,7 @@ def main():
         )[:2]:
         print('=' * 80)
         print(name)
-        results.append(give_me_classifier(clf, train_set, train_label, test_set, test_labels))
+        results.append(give_me_classifier(train_set, train_label, test_set, test_labels, clf))
     print "=" * 80
     for res in results:
         print 'Classifier: %s, its train accuracy = %2.3f, test accuracy = %2.3f' % res[0], res[1], res[2]
