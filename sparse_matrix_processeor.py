b1cb1453174c6f1262f4b5bdf02a387f45540dc5
binking <jiangzhibin2014.xujie@gmail.com>
2016-12-19 16:34:36 +0800

    multinomial NB

--- sparse_matrix_processeor.py
+++ sparse_matrix_processeor.py
@@ -3,16 +3,14 @@
 #  1. Classification of text documents using sparse features
 #        http://scikit-learn.org/stable/auto_examples/text/document_classification_20newsgroups.html
 #  2. 
-
 import time
 import jieba
 import string
 import numpy as np
 import pandas as pd
-from graphlab import SFrame
 from pandas import DataFrame
 from scipy.sparse import csr_matrix
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 from sklearn.naive_bayes import (
 	GaussianNB,
 	MultinomialNB
@@ -56,9 +54,10 @@ def format_bow_csr_matrix(docs):
             data.append(1)
         indptr.append(len(indices))  # len(indices) means hwo many nnz in the col
     tf_csr_matrix = csr_matrix( (data, indices, indptr), dtype=int)
-    idf_vector = np.array([freq for freq, _ in sorted(idf_index2freq, key=lambda x:x[0])])
+    idf_vector = np.array([freq for _, freq in sorted(idf_index2freq.items(), key=lambda x:x[0])])
     print idf_vector
     return tf_csr_matrix, idf_vector
+
 
 def load_tokens_from_file(filename):
     docs = []
@@ -66,13 +65,13 @@ def load_tokens_from_file(filename):
     # <class 'pandas.core.frame.DataFrame'>
     # <class 'pandas.core.series.Series'> to <type 'numpy.ndarray'> : labels.values
     labels = []
-    data_frame = pd.read_csv(filename, delimiter='|', header=None, names=['topic', 'content', 'label'], nrows=100)
+    data_frame = pd.read_csv(filename, delimiter=',', nrows=1000)
     for i, doc in enumerate(data_frame['content']):
         if isinstance(doc, float):
             print i, '-th unkown doc: ', doc
             continue
         docs.append(tokenize_text(doc))
-        label.append(data_frame['label'])
+        labels.append(data_frame['class'][i])
     # import ipdb; ipdb.set_trace()  
     return docs, np.array(labels)
 
@@ -80,29 +79,29 @@ def load_tokens_from_file(filename):
 def main():
     load_time = time.time()
     # Preprocess text and generte csr matrix
-    documents, labels = load_tokens_from_file('user_labels_simple_dataset.csv')
-    bag_of_words_csr = format_bow_csr_matrix(documents)
-    size, dims = bag_of_words_csr.shape
-    train_set = bag_of_words_csr[:int(0.8*size), :]  # slice sparse matrix: csc[:,indices], csr[indices,:]
+    documents, labels = load_tokens_from_file('topic_classifier_simple_dataset.csv')
+    term_freq, idf_vector = format_bow_csr_matrix(documents)
+    td_idf_matrix = term_freq.dot(1/(1.0+idf_vector))
+    size, dims = td_idf_matrix.shape
+    train_set = td_idf_matrix[:int(0.8*size), :]  # slice sparse matrix: csc[:,indices], csr[indices,:]
     train_label = labels[:int(0.8*size)]
     train_time = time.time()
     print '\nPreprocess cost %d seconds' % (train_time - load_time)
 
     # Train model
-    gnb_model = GaussianNB()
-    gnb_model.fit(train_set.todense(), train_label)
-    prediction = gnb_model.predict(train_set.todense())  # predict need dense matrix
+    mnb_model = MultinomialNB()
+    mnb_model.fit(train_set, train_label)
+    prediction = mnb_model.predict(train_set)  # predict need dense matrix
     accuracy = np.sum(prediction==train_label)*1.0 / len(train_label)
     print "Accuracy is %f " % accuracy
     print '\nTrain data spent %d seconds' % (time.time() - train_time)
 
     # Test model
-    test_set = bag_of_words_csr[int(0.8*size):, :]
+    test_set = td_idf_matrix[int(0.8*size):, :]
     test_labels = labels[int(0.8*size):]
-    test_pred = gnb_model.predict(test_set.todense())
+    test_pred = mnb_model.predict(test_set)
     test_accuracy = np.sum(test_pred == test_labels)*1.0 / len(test_labels)
     print 'Test set accuracy: %f' % test_accuracy
-
 
 
 if __name__=='__main__':
